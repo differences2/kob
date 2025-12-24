103bd21edd69c19b3491c3976d5544a86d650b2c
YongLiang Cheng <2312854733@qq.com>
2025-12-24 19:45:48 +0800

    create backend

---
+++ backend/src/main/java/org/kob/backend/controller/pk/IndexController.java
@@ -0,0 +1,12 @@
+package org.kob.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pk/")
+public class IndexController {
+
+
+
+}
